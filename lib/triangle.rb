af511e4fcd0c0726ebc2d7d425b6d04052f7a773
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-17 00:09:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :s1,:s2,:s3
+
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+def kind
+ if (@s1 + @s2)  <= @s3 ||
+end
+
+class TriangleError < StandardError
+
+end
+
+
 end
